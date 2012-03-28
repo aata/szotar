480c9f29aefe43d05b00616f4d89c41a7796ec71
lee@asztal.net <lee@asztal.net@12f24b11-804c-0410-bc5a-db030c1e9a4f>
2012-03-28 10:14:54 +0000

    These files should have been added many revisions ago.

---
+++ trunk/Client/Szotar.WindowsForms/Controls/DictionarySectionUI.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Szotar.WindowsForms.Importing;
+
+namespace Szotar.WindowsForms.Controls {
+    public partial class DictionarySectionUI : UserControl, IImporterUI<IBilingualDictionary> {
+        IDictionarySectionImporter importer;
+        DualSectionImporter dualSection;
+
+        public DictionarySectionUI(DualSectionImporter dualSection, IDictionarySectionImporter importer) {
+            InitializeComponent();
+
+            this.dualSection = dualSection;
+            this.importer = importer;
+        }
+
+        private void browse_Click(object sender, EventArgs e) {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "All files (*.*)|*.*";
+            ofd.Multiselect = false;
+
+            if (ofd.ShowDialog() == DialogResult.OK) {
+                fileName.Text = ofd.FileName;
+            }
+        }
+
+        private void fileSelectNext_Click(object sender, EventArgs e) {
+            OnFinished();
+        }
+
+        #region IImporterUI
+        public void Apply() {
+            dualSection.SetImporters(importer, fileName.Text, null, string.Empty, generateSecondHalf.Checked);
+        }
+
+        public event EventHandler Finished;
+        private void OnFinished() {
+            EventHandler temp = Finished;
+            if (temp != null)
+                temp(this, EventArgs.Empty);
+        }
+
+        public IImporter<IBilingualDictionary> Importer {
+            get { return dualSection; }
+        }
+        #endregion
+
+        private void fileName_TextChanged(object sender, EventArgs e) {
+            fileSelectNext.Enabled = fileName.TextLength > 0;
+        }
+    }
+}
